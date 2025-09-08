4553caf0366c78da8bc792f3318cf9b104cde955
Seditoir <schoenmakersuni@gmail.com>
2025-09-08 11:29:14 +0200

    Created an function to turn the last 6 columns form character to numeric
    A function that converts the last six columns to numeric in each data frame within a list.
    these columns contain the measurement data and have to be numeric for further merging.

---
+++ src/skml_data_class_corrector.R
@@ -0,0 +1,21 @@
+data_class_correction_function <- function(dataframe_list){
+  
+  dataframe_corrected <-list()
+  
+  for(i in 1:length(dataframe_list)){
+    
+    indvidual_dataframe <- dataframe_list[[1]]
+    last_columns <- (ncol(indvidual_dataframe)-5):ncol(indvidual_dataframe) 
+    
+    for(j in last_columns){
+      indvidual_dataframe[[j]] <- as.numeric(indvidual_dataframe[[j]])
+    }
+    
+    dataframe_corrected[[i]] <- indvidual_dataframe
+    
+  }
+  
+  return(dataframe_corrected)
+}
+
+
