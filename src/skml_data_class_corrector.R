0125027b4a15f0395f92f88a8fb1da46feba91d1
Seditoir <schoenmakersuni@gmail.com>
2025-09-09 11:13:42 +0200

    minor fix to skml_data_class_corrector.R
    Fixed for loop indexing

--- src/skml_data_class_corrector.R
+++ src/skml_data_class_corrector.R
@@ -4,7 +4,7 @@ skml_data_class_correction_function <- function(dataframe_list){
   
   for(i in 1:length(dataframe_list)){
     
-    indvidual_dataframe <- dataframe_list[[1]]
+    indvidual_dataframe <- dataframe_list[[i]]
     last_columns <- (ncol(indvidual_dataframe)-5):ncol(indvidual_dataframe) 
     
     for(j in last_columns){
