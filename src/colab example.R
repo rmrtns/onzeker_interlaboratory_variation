5a1820722ebb093d478ba2b4581a1bc75343cdd5
Ruben <ruben@example.com>
2025-11-19 14:31:17 +0100

    CoLab implementation

--- src/colab example.R
+++ src/colab example.R
@@ -1,18 +1,20 @@
 library(dplyr)
 library(mcr)
+library(pROC)
 source("src/predict_CoLab.R")
 
 
 # Import and pre-process --------------------------------------------------
-
-
+                             
 dataZMC <- read.csv("data/CoLab_externe_validatie_Zuyderland.csv")
+dataZMC$CoLab_binary <- get_binary_prediction_CoLab(dataZMC$CoLab_lp)
 
 # De-transform log10 variables and removed log10_ from columns names
 log10_cols <- grep("log10", names(dataZMC), value = T)
 dataZMC_nolog <- dataZMC %>%
   mutate(across(all_of(log10_cols), ~ 10 ^ .x)) %>% 
-  rename_with(~ gsub("log10_", "", log10_cols), .cols = log10_cols)
+  rename_with(~ gsub("log10_", "", log10_cols), .cols = log10_cols) %>% 
+  select(!c("CoLab_lp", "CoLab_score"))
 
 skml <- read.csv("data/skml_merged.csv")
 # Select one cluster from each participant
@@ -46,9 +48,38 @@ paba_regs <- skml_one_ptp %>%
 # Create datasets ---------------------------------------------------------
 
 
+# Add bias (slope and intercept) based on Passing-Bablok regression
+#
+# orig_dataset         is the (reference) dataset
+# pb_reg_data_ctr      is the PaBa regression data from a specific participant 
+#                      & cluster
+# variable_dictionary  is a named vector where the elements are equal to the
+#                      Bepaling string in the SKML data and the names correspond
+#                      to the covariate string in the orig_dataset, e.g.
+#                      "AlkFosf_BV" = "Alk. Fosfatase", if the covariate
+#                      does not appear in the SKML data it should be NA, e.g.
+#                      "EosAbs_BV" = NA.
 
-ptps <- paba_regs$ptp[1:10]
+createBiasedData <- function(orig_dataset, skml_paba_reg_ptp_ctr,
+                             variable_dictionary) {
+  
+  biased_dataset <- orig_dataset
+  # Do for each variable 
+  for (variable in names(variable_dictionary)) {
+    sklm_name <- variable_dictionary[[variable]]
+    # Check if variable is present in SKML data
+    if (!is.na(sklm_name) & sklm_name %in% skml_paba_reg_ptp_ctr$Bepaling) {
+      pbreg_bepaling <- subset(skml_paba_reg_ptp_ctr, Bepaling == sklm_name)
+      orig_results <- orig_dataset[, variable]
+      # Add center specific bias (slope & intercept )
+      biased_dataset[, variable] <- pbreg_bepaling[['Intercept']] + 
+        pbreg_bepaling[['Slope']] * orig_results
+    }
+  }
+  return(biased_dataset)
+}
 
+# Translation of CoLab variables to SKML bepalingen
 
 covars_w_skml_name <- c("Erytrocyten_BV" = "Erytrocyten", 
                         "Leukocyten_BV" = "Leukocyten",
@@ -62,23 +93,33 @@ covars_w_skml_name <- c("Erytrocyten_BV" = "Erytrocyten",
                         "CRP_BV" = "CRP",
                         "age" = NA)
 
+# Split PaBa-regression results by participant 
+paba_regs_split <- split(paba_regs, paba_regs$ptp)
+
+# Create biased data by participant
+ptp_biasedData <- lapply(paba_regs_split, createBiasedData, 
+                         orig_dataset = dataZMC_nolog,
+                         variable_dictionary = covars_w_skml_name)
+
+# Calculate CoLab-lp and score by dataset
+ptp_results <- lapply(ptp_biasedData, function(x){ 
+  x$CoLab_lp <- get_lp_prediction_CoLab(x)
+  x$CoLab_score <- get_ordinal_prediction_CoLab(x$CoLab_lp)
+  x$CoLab_binary <- get_binary_prediction_CoLab(x$CoLab_lp)
+  x})
 
 
+ptp_rocs <- lapply(ptp_results, function(x) 
+  roc(x, "outcome", "CoLab_score", direction = "<"))
 
-lapply(names(covars_w_skml_name), 
-       function(x) addBias(orig_dataset =  dataZMC_nolog,
-                   bepaling_naam_in_data = x, 
-                   bepaling_naam_in_skml = covars_w_skml_name[[x]],
-                   pbreg_data_ptp = subset(paba_regs, ptp == 1 & ctr == 4)))
-addBias <- function(orig_dataset, bepaling_naam_in_data,
-                    bepaling_naam_in_skml, pbreg_data_ptp){
-  if (!is.na(bepaling_naam_in_skml) | !(bepaling_naam_in_skml %in% pbreg_data_ptp$Bepaling)) { 
-    pbreg_bepaling <- subset(pbreg_data_ptp, Bepaling == bepaling_naam_in_skml)
-    orig_resultaten <- subset(orig_dataset, select = bepaling_naam_in_data)
-    pbreg_bepaling[['Intercept']] + pbreg_bepaling[['Slope']] * orig_resultaten
-    print(bepaling_naam_in_data)
-  } else {
-    return(orig_dataset)
-  }
-}
+ptp_aucs <- sapply(ptp_rocs, function(x) x$auc)
+quantile(unlist(ptp_aucs), probs = c(0.025, 0.5, 0.975))
 
+ptp_coords <- lapply(ptp_rocs, coords, x = 5)
+ptp_coords <- do.call(rbind, ptp_coords)
+quantile(ptp_coords$specificity, probs = c(0.025, 0.5, 0.975))
+quantile(ptp_coords$sensitivity, probs = c(0.025, 0.5, 0.975))
+
+orig_roc <- roc(dataZMC, "outcome", "CoLab_score", direction = "<")
+orig_roc$auc
+coords(orig_roc, x = 5)
--- src/predict_CoLab.R
+++ src/predict_CoLab.R
@@ -3,6 +3,7 @@
 
 
 get_lp_prediction_CoLab <- function(X.unscaled){
+  
   coefs <- c("(Intercept)" = -6.885000,
              "Erytrocyten_BV" = 0.937900,
              "Leukocyten_BV" = -0.129800,
@@ -16,27 +17,34 @@ get_lp_prediction_CoLab <- function(X.unscaled){
              "CRP_BV" = 0.002560,
              "age" = 0.002275)
   X <- as.data.frame(X.unscaled)
+  
   coef.names.no.int <- names(coefs)["(Intercept)" != names(coefs)]
-  if (!all(coef.names.no.int %in% names(X))) {
+  
+  # Remove log10_prefix as we input untransformed values
+  coef.names.no.int.no.log <- sub("^log10_", "", coef.names.no.int)
+  
+  if (!all(coef.names.no.int.no.log %in% names(X.unscaled))) {
     stop(paste(c("Not all coef variables in X:",
                  coef.names.no.int[!coef.names.no.int %in% names(X)]), 
                collapse = " "))
     paste("Not all coef variables in X:",
           coef.names.no.int[!coef.names.no.int %in% names(X)])
   }
-  X.ordered <- subset(X, select = coef.names.no.int)
+  X.ordered <- subset(X, select = coef.names.no.int.no.log)
+  
+  # Do log transform on log transformed variables
+  log10cols <- coef.names.no.int.no.log[grep("log10", coef.names.no.int)]
+  X.ordered[, log10cols] <- lapply(X.ordered[, log10cols], log10)
   lp <- rowSums(mapply('*', X.ordered, coefs[coef.names.no.int]))
   lp + coefs["(Intercept)"]
 }
 
-get_ordinal_prediction_CoLab <- function(X.unscaled){
-  lps <- get_lp_prediction_CoLab
+get_ordinal_prediction_CoLab <- function(CoLab_lps){
   cutoffs <- c( -1.64, -2.34, -3.29, -4.03, -5.83)
-  cut(lps, c(Inf, cutoffs, -Inf), labels = seq(0, length(cutoffs)), 
+  cut(CoLab_lps, c(Inf, cutoffs, -Inf), labels = seq(0, length(cutoffs)), 
       ordered_result = T)
 }
 
-get_binary_prediction_CoLab <- function(X.unscaled){
-  lps <- get_lp_prediction_CoLab
-  lps >= -1.64
+get_binary_prediction_CoLab <- function(CoLab_lps){
+  CoLab_lps >= -1.64
 }
