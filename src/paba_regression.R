656cec6c5e19acb49f36eeab9bbc5a111126b50f
Seditoir <schoenmakersuni@gmail.com>
2025-12-11 10:35:16 +0100

    small changes

--- src/paba_regression.R
+++ src/paba_regression.R
@@ -1,21 +1,7 @@
 library(mcr)
 library(dplyr)
 
-# paba.reg.fun <- function(ReferenceMethod, TestMethod){
-#   
-#   PB.reg <- mcr::mcreg(ReferenceMethod,
-#                        TestMethod,
-#                        method.reg = "PBequi",
-#                        method.ci = "analytical")
-#   
-#   coef <- getCoefficients(PB.reg)
-#   
-#   return(data.frame(
-#     Intercept = coef["Intercept", "EST"],
-#     Slope = coef["Slope", "EST"]
-#   ))
-#   
-# }
+
 
 paba.reg.fun <- function(ReferenceMethod, TestMethod,N){
   # Minimum sample size is N
@@ -24,9 +10,9 @@ paba.reg.fun <- function(ReferenceMethod, TestMethod,N){
                          TestMethod,
                          method.reg = "PBequi",
                          method.ci = "analytical")
-    
+
     coef <- getCoefficients(PB.reg)
-    
+
     return(data.frame(
       Intercept = coef["Intercept", "EST"],
       Slope = coef["Slope", "EST"]))
@@ -34,3 +20,23 @@ paba.reg.fun <- function(ReferenceMethod, TestMethod,N){
     return(data.frame(Intercept = NA, Slope = NA))
   }
 }
+
+
+# paba.reg.fun <- function(ReferenceMethod, TestMethod,N){
+#   # Minimum sample size is N
+#   if (length(ReferenceMethod) >= N) {
+#     PB.reg <- mcr::mcreg(ReferenceMethod,
+#                          TestMethod,
+#                          method.reg = "PBequi",
+#                          method.ci = "analytical")
+#     
+#     coef <- getCoefficients(PB.reg)
+#     
+#     return(data.frame(
+#       var = paste("y ~ ", Slope = round(coef["Slope", "EST"],3), "*x+", round(coef["Intercept", "EST"],3), sep = ""
+#       )))
+#   } else {
+#     return(data.frame(Intercept = NA, Slope = NA))
+#   }
+# }
+
