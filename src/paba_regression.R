29c8f00851b19e269c74c3111b96f226b8212b91
Seditoir <schoenmakersuni@gmail.com>
2025-12-17 09:44:34 +0100

    - Reworked the predict_colab score to be able to with the markdown file
    Removed the formula version of the bias funtion in the paba.reg.fun

--- src/paba_regression.R
+++ src/paba_regression.R
@@ -21,22 +21,3 @@ paba.reg.fun <- function(ReferenceMethod, TestMethod,N){
   }
 }
 
-
-# paba.reg.fun <- function(ReferenceMethod, TestMethod,N){
-#   # Minimum sample size is N
-#   if (length(ReferenceMethod) >= N) {
-#     PB.reg <- mcr::mcreg(ReferenceMethod,
-#                          TestMethod,
-#                          method.reg = "PBequi",
-#                          method.ci = "analytical")
-#     
-#     coef <- getCoefficients(PB.reg)
-#     
-#     return(data.frame(
-#       var = paste("y ~ ", Slope = round(coef["Slope", "EST"],3), "*x+", round(coef["Intercept", "EST"],3), sep = ""
-#       )))
-#   } else {
-#     return(data.frame(Intercept = NA, Slope = NA))
-#   }
-# }
-
--- src/variables_colab.R
+++ src/variables_colab.R
@@ -1,26 +1,42 @@
-selected_variables_colab <- c("Erytrocyten_BV" = "Erytrocyten", 
-                             "Leukocyten_BV" = "Leukocyten",
-                             "EosAbs_BV" = NA,
-                             "BasoAbs_BV" = NA, 
-                             "BilirubineTotaal_BV" = "Bilirubine",
-                             "LD_BV" = "LD",
-                             "AlkFosf_BV" = "Alk. Fosfatase",
-                             "GGT_BV" = "Gamma-GT",
-                             "Albumine_BV" = "Albumine",
-                             "CRP_BV" = "CRP",
-                             "age" = NA
+selected_variables_colab <- c("Erytrocyten_BV" = "Erytrocyten_BV", 
+                             "Leukocyten_BV" = "Leukocyten_BV",
+                             "EosAbs_BV" = "EosAbs_BV",
+                             "BasoAbs_BV" = "BasoAbs_BV", 
+                             "BilirubineTotaal_BV" = "BilirubineTotaal_BV",
+                             "LD_BV" = "LD_BV",
+                             "AlkFosf_BV" = "AlkFosf_BV",
+                             "GGT_BV" = "GGT_BV",
+                             "Albumine_BV" = "Albumine_BV",
+                             "CRP_BV" = "CRP_BV",
+                             "age" = "age"
 )
 
 
+
+# 
+# uncertain_variables_indicator_colab <- c(
+#   TRUE,
+#   TRUE,
+#   FALSE,
+#   FALSE,
+#   TRUE,
+#   TRUE,
+#   TRUE,
+#   TRUE,
+#   TRUE,
+#   TRUE,
+#   FALSE)
+
+
 uncertain_variables_indicator_colab <- c(
-  TRUE,
+  FALSE,
   TRUE,
   FALSE,
   FALSE,
-  TRUE,
-  TRUE,
-  TRUE,
-  TRUE,
-  TRUE,
-  TRUE,
+  FALSE,
+  FALSE,
+  FALSE,
+  FALSE,
+  FALSE,
+  FALSE,
   FALSE)
