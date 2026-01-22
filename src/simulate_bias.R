f3a98abc327272d8562145bc11020b9321cf785f
Ruben <ruben@example.com>
2026-01-22 11:08:41 +0100

    Removed redundant function

--- src/simulate_bias.R
+++ src/simulate_bias.R
@@ -12,7 +12,7 @@ create_constant_data <- function(data, variables){
 simulate_bias <- function(data, identifier, variables, bias_factors, bias_intercepts){ 
   simulated_bias_per_database <- data.frame(identifier = data[[identifier]])
   simulated_bias_per_database <- simulated_bias_per_database %>% rename(!!sym(identifier) := identifier)
-  for (variable_index in 1:length(variables)){
+  for (variable_index in 1:length(variables)) {
     simulated_bias_per_variable <- data[[variables[variable_index]]]*bias_factors[variable_index] + bias_intercepts[variable_index]
     simulated_bias_per_database <- simulated_bias_per_database %>% 
       mutate(!!sym(variables[variable_index]) := simulated_bias_per_variable)
@@ -20,7 +20,7 @@ simulate_bias <- function(data, identifier, variables, bias_factors, bias_interc
   return(simulated_bias_per_database)
 }
 
-
-multiply_variable_by_factor <- function(original, factor){
-  return(original * factor)
-}
+# Redundant
+# multiply_variable_by_factor <- function(original, factor){
+#   return(original * factor)
+# }
