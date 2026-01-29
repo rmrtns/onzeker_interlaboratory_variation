63dbf2dc7ffb671ffcb7ed1943f0c06cae3bacb1
Seditoir <schoenmakersuni@gmail.com>
2026-01-29 14:36:11 +0100

    change the bias table function to only output an complete bias table for the variables neccesary.

--- src/calculate_bias_table.R
+++ src/calculate_bias_table.R
@@ -1,9 +1,12 @@
+library("dplyr")
+library("tidyr")
+
 get_intercept_table <- function(data_SKML, reference_ptp, skml_names, non_bias_vec){
-  df1 <- Regression_with_ref_centre_function(data_SKML, reference_ptp)
+  df1 <- regression_with_ref_centre_function(data_SKML, reference_ptp)
   
   # get the bias estimates in the wide format
   df2 <- df1 %>%
-    group_by(ptp,ctr) %>%
+    group_by(ptp) %>%
     select(c(Bepaling, New_Intercept )) %>%
     pivot_wider(names_from = Bepaling, values_from = c(New_Intercept)) 
   
@@ -12,17 +15,18 @@ get_intercept_table <- function(data_SKML, reference_ptp, skml_names, non_bias_v
     select(skml_names) %>%
     mutate(!!!setNames(as.list(rep(NA, length(non_bias_vec))), non_bias_vec))
   
-  return(df3)
+  
+  df4 <- extract_only_bias_variables_function(df3, skml_names)
+  return(df4)
   
 }
 
 get_slope_table <- function(data_SKML,  reference_ptp, skml_names, non_bias_vec){
-  
-  df1 <- Regression_with_ref_centre_function(data_SKML, reference_ptp)
+  df1 <- regression_with_ref_centre_function(data_SKML, reference_ptp)
   
   # get the bias estimates in the wide format
   df2 <- df1 %>%
-    group_by(ptp,ctr) %>%
+    group_by(ptp) %>%
     select(c(Bepaling,New_Slope )) %>%
     pivot_wider(names_from = Bepaling, values_from = c(New_Slope)) 
   
@@ -31,15 +35,19 @@ get_slope_table <- function(data_SKML,  reference_ptp, skml_names, non_bias_vec)
     select(skml_names) %>%
     mutate(!!!setNames(as.list(rep(NA, length(non_bias_vec))), non_bias_vec))
   
-  return(df3)
+  df4 <- extract_only_bias_variables_function(df3, skml_names)
+  return(df4)
   
 }
 
 # set one centre as the refrence centre 
-Regression_with_ref_centre_function <- function(data_SKML, reference_ptp){
-
+regression_with_ref_centre_function <- function(data_SKML, reference_ptp){
+  df0 <- data_SKML %>%
+    group_by(Bepaling, ptp) %>%
+    filter(ctr == min(ctr))
+  
   # seperate the reference ptp and designate the slope and intercept as the reference slope and intercept
-  df1 <- subset(data_SKML, ptp == reference_ptp)
+  df1 <- subset(df0, ptp == reference_ptp)
   df1 <- df1 %>% 
     rename("Ref_Intercept" = "Intercept") %>% 
     rename("Ref_Slope" = "Slope") %>% 
@@ -47,10 +55,20 @@ Regression_with_ref_centre_function <- function(data_SKML, reference_ptp){
     select(-ptp, -ctr)
   
   # calculate the new slope and intercept with the reference bias
-  df2 <- left_join(subset(data_SKML, ptp != reference_ptp), 
+  df2 <- left_join(subset(df0, ptp != reference_ptp), 
                    df1, by = "Bepaling") %>% 
     mutate(New_Intercept = -((Ref_Slope*Ref_Intercept)/Ref_Slope) + Intercept) %>% 
     mutate(New_Slope = Slope/Ref_Slope) 
   
   return(df2)
 }
+
+extract_only_bias_variables_function <- function(data, skml_names){
+  df1 <- data %>% select(skml_names)
+  df2 <- df1 %>%
+    rowwise() %>%
+    filter(sum(is.na(c_across(everything()))) == 0)
+  return(df2)
+  
+}
+
