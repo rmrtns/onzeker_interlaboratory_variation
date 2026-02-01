195ec227c2ee1bdaf2e9e4ea8dbf4b7af93a34f3
rmrtns <109623126+rmrtns@users.noreply.github.com>
2026-02-01 14:45:35 +0100

    Update variables_colab.R

--- src/variables_colab.R
+++ src/variables_colab.R
@@ -1,14 +1,15 @@
-selected_variables_colab <- c("Erytrocyten_BV" = "Erytrocyten_BV", 
-                             "Leukocyten_BV" = "Leukocyten_BV",
-                             "EosAbs_BV" = "EosAbs_BV",
-                             "BasoAbs_BV" = "BasoAbs_BV", 
-                             "BilirubineTotaal_BV" = "BilirubineTotaal_BV",
-                             "LD_BV" = "LD_BV",
-                             "AlkFosf_BV" = "AlkFosf_BV",
-                             "GGT_BV" = "GGT_BV",
-                             "Albumine_BV" = "Albumine_BV",
-                             "CRP_BV" = "CRP_BV",
-                             "age" = "age"
+selected_variables_colab <- c(
+  "Erytrocyten_BV" = "Erytrocyten_BV", 
+  "Leukocyten_BV" = "Leukocyten_BV",
+  "EosAbs_BV" = "EosAbs_BV",
+  "BasoAbs_BV" = "BasoAbs_BV", 
+  "BilirubineTotaal_BV" = "BilirubineTotaal_BV",
+  "LD_BV" = "LD_BV",
+  "AlkFosf_BV" = "AlkFosf_BV",
+  "GGT_BV" = "GGT_BV",
+  "Albumine_BV" = "Albumine_BV",
+  "CRP_BV" = "CRP_BV",
+  "age" = "age"
 )
 
 
