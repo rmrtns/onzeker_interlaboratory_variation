3541d886c63fe86f5702ecc5aea1f071454a8a7a
rmrtns <remy.martens@outlook.com>
2026-02-09 21:15:51 +0100

    updated markdown templates and simplified save function

--- src/save_output.R
+++ src/save_output.R
@@ -8,7 +8,6 @@ save_database_data <- function(data, base_dir, laboratory){
 
 save_summary <- function(summary_object, base_dir, model_name, analysis_type, population, description){
   dir.create(base_dir, recursive = TRUE, showWarnings = FALSE)
-  
   write.csv(
     summary_object,
     paste0(base_dir, model_name, "_", analysis_type, "_", population, "_", description, ".csv"),
