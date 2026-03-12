b8a7d9b44324aec0ffbfbeb1eb15a4b49140a107
Ruben Deneer <ruben.deneer@catharinaziekenhuis.nl>
2026-03-12 17:34:48 +0100

    Finished linearity assumption testing

--- src/skml_data_to_markdown_bias.R
+++ src/skml_data_to_markdown_bias.R
@@ -3,41 +3,45 @@ library(tidyr)
 library(mcr)
 library(pROC)
 
-# functions 
-paba.reg.fun <- function(ReferenceMethod, TestMethod){
-  # Minimum sample size is 16
-  if (length(ReferenceMethod) >= N) {
-    PB.reg <- mcr::mcreg(ReferenceMethod,
-                         TestMethod,
-                         method.reg = "PBequi",
-                         method.ci = "analytical")
-    
-    coef <- getCoefficients(PB.reg)
-    cumsum.stats <- calcCUSUM(PB.reg)
-    H <- with(cumsum.stats, max.cusum/sqrt(nNeg + 1))
-    return(data.frame(
-      Intercept = coef["Intercept", "EST"],
-      Slope = coef["Slope", "EST"],
-      H = round(H, 2),
-      Lin.test.reject = H >= 1.36))     # H <= 1.36 for linearity
-  } else {
-    return(data.frame(Intercept = NA, Slope = NA))
-  }
-}
+# minimal amount of observations for paba method
+N_min = 16 
 
 # read the merged skml file 
 #   If file doesn't exist, run:
 #   source("src/new skml import.R")
 skml <- read.csv("data/skml_merged.csv")
 
-# minimal amount of observations for paba method
-N = 16 
+
+paba.reg.fun <- function(ReferenceMethod, TestMethod){
+  # Minimum sample size is 16
+  N <-length(ReferenceMethod)
+  if (N >= N_min) {
+    PB.reg <- mcr::mcreg(ReferenceMethod,
+                         TestMethod,
+                         method.reg = "PBequi",
+                         method.ci = "analytical")
+    
+    coef <- getCoefficients(PB.reg)
+    cusum.stats <- calcCUSUM(PB.reg)
+    H <- with(cusum.stats, max.cusum/sqrt(nNeg + 1))
+    n <- cusum.stats$nNeg + 1
+    p_val <- 1 - stats:::pkolmogorov(H/sqrt(n), size = n, exact = F)
+    return(data.frame(
+      Intercept = coef["Intercept", "EST"],
+      Slope = coef["Slope", "EST"],
+      Lin_test_p = p_val,
+      N = N))
+    } else {
+    return(data.frame(Intercept = NA, Slope = NA, Lin_test_p = NA, N = NA))
+  }
+}
 
 # perfom the paba regression
 paba_regs_wide <- skml %>% 
   group_by(Bepaling, ptp, ctr) %>% 
   do(paba.reg.fun(.$ConsensusWaarde, .$Resultaat)) %>%
-  pivot_wider(names_from = Bepaling, values_from = c(Intercept, Slope, H, Lin.test.reject)) %>%
+  pivot_wider(names_from = Bepaling, values_from = c(Intercept, Slope, 
+                                                     Lin_test_p, N)) %>%
   mutate(name = paste("ptp", ptp, "ctr", ctr, sep = "_"))
 
 # output in csv file
