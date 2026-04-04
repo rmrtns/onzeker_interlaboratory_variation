bff48f17785917407a8d1b9976a2f032921c004d
rmrtns <remy.martens@outlook.com>
2026-04-04 19:52:43 +0200

    Updated colab data preperation

--- src/prepare_dataset_colab.R
+++ src/prepare_dataset_colab.R
@@ -11,6 +11,38 @@ colab_variable_de_log <- function(data){
   return(data_nolog)
 }
 
+# Import dataset.
+colab_with_log <- read.csv('data/CoLab_externe_validatie_Zuyderland_met_datum.csv')
+
+# Prepare dataset.
+colab_wo_log <- colab_variable_de_log(colab_with_log)
+colab_wo_log <- colab_wo_log %>% 
+  mutate(
+    ER_date = lubridate::as_date(ER_date),
+    visit = row_number()
+  ) %>%
+  filter(age >= 18)
+
+# Split dataset according to prevalence.
+colab_low_prevalence <- colab_wo_log
+  
+colab_high_prevalence <- colab_wo_log %>%
+  filter(ER_date >= lubridate::as_date("2020-03-01"))
+
+# Save datasets.
+
+write.csv(
+  colab_low_prevalence,
+  paste0('data/colab_ed_low_prevalence.csv'),
+  row.names = FALSE
+)
+
+write.csv(
+  colab_high_prevalence,
+  paste0('data/colab_ed_low_prevalence.csv'),
+  row.names = FALSE
+)
+
 # Prepare colab ed dataset (low prevalence).
 colab_ed_low_with_log <- read.csv('data/Data_colab_ED_low_prevalence.csv')
 
