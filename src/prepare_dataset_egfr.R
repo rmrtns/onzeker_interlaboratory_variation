2be0961c8d12e842aa836969b7835959aaafaf41
Seditoir <schoenmakersuni@gmail.com>
2026-04-16 15:31:08 +0200

    adapted prepare_dataset_egfr.r to work with the long format source files

--- src/prepare_dataset_egfr.R
+++ src/prepare_dataset_egfr.R
@@ -5,10 +5,18 @@ library(dplyr)
 source('src/variables_egfr.R', local = variables <- new.env())
 
 # Prepare egfr gp dataset.
-egfr_gp <- read.csv('data/JuistheidCKD-HA(Remy)_251017_anonymised_wide_format.csv')
-egfr_gp_wo_missing <- egfr_gp %>%
+egfr_gp <- read.csv('data/JuistheidCKD-HA(Remy)_251017_anonymised.csv')
+egfr_gp$X.3 <- as.numeric(egfr_gp$X.3)
+egfr_gp_wide <- pivot_wider(egfr_gp,
+                                  id_cols = c(ID,AfnD,AfnT,Gesl,Leeft ),
+                                  names_from = X,
+                                  values_from = X.3,
+                                  values_fn = list(X.3 = mean))
+
+egfr_gp_wo_missing <- egfr_gp_wide %>%
   filter(complete.cases(across(all_of(variables$selected_variables_egfr)))) %>%
-  mutate(Gesl = replace_values(Gesl, "M" ~ 1, "V" ~ 0))
+  mutate(Gesl = recode(Gesl, "M" = 1, "V" = 0)) %>%
+  mutate(BezoekersID = row_number()) 
 
 write.csv(
   egfr_gp_wo_missing,
@@ -17,10 +25,17 @@ write.csv(
 )
 
 # Prepare egfr int dataset.
-egfr_int <- read.csv('data/JuistheidCKD-Int(Remy)_251030_anonymised_wide_format.csv')
-egfr_int_wo_missing <- egfr_int %>%
+egfr_int <- read.csv('data/JuistheidCKD-Int(Remy)_251030_anonymised.csv')
+egfr_int$X.3 <- as.numeric(egfr_int$X.3)
+egfr_int_wide <- pivot_wider(egfr_int,
+                            id_cols = c(ID,AfnD,AfnT,Gesl,Leeft ),
+                            names_from = X,
+                            values_from = X.3,
+                            values_fn = list(X.3 = mean))
+egfr_int_wo_missing <- egfr_int_wide %>%
   filter(complete.cases(across(all_of(variables$selected_variables_egfr)))) %>%
-  mutate(Gesl = replace_values(Gesl, "M" ~ 1, "V" ~ 0))
+  mutate(Gesl = recode(Gesl, "M" = 1, "V" = 0)) %>%
+  mutate(BezoekersID = row_number()) 
 
 write.csv(
   egfr_int_wo_missing,
