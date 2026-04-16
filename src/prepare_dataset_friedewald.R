384ae2492a62d78a69806a5d5b744a3320f27af0
Seditoir <schoenmakersuni@gmail.com>
2026-04-16 11:27:27 +0200

    Update the the prepare_dataset_friedewald.R file containing all the pre processing steps.

--- src/prepare_dataset_friedewald.R
+++ src/prepare_dataset_friedewald.R
@@ -5,10 +5,20 @@ library(dplyr)
 source('src/variables_friedewald.R', local = variables <- new.env())
 
 # Prepare friedewald gp dataset.
-friedewald_gp <- read.csv('data/JuistheidFRIE-HA(Remy)_251029_anonymised_wide_format.csv')
-friedewald_gp_wo_missing <- friedewald_gp %>%
+friedewald_gp <- read.csv('data/JuistheidFRIE-HA(Remy)_251029_anonymised.csv')
+friedewald_gp$X.3 <- as.numeric(friedewald_gp$X.3)
+friedewald_gp_wide <- pivot_wider(friedewald_gp,
+                                  id_cols = c(ID,AfnD,AfnT,Gesl,Leeft ),
+                                  names_from = X,
+                                  values_from = X.3,
+                                  values_fn = list(X.3 = mean))
+friedewald_gp_wide <- friedewald_gp_wide %>%
+  rename_with(~ gsub("-", ".", .x))
+friedewald_gp_wo_missing <- friedewald_gp_wide %>%
   filter(complete.cases(across(all_of(variables$selected_variables_friedewald)))) %>%
-  mutate(Gesl = replace_values(Gesl, "M" ~ 1, "V" ~ 0))
+  mutate(Gesl = recode(Gesl, "M" = 1, "V" = 0)) %>%
+  mutate(BezoekersID = row_number())
+
 
 write.csv(
   friedewald_gp_wo_missing,
@@ -17,10 +27,19 @@ write.csv(
 )
 
 # Prepare friedewald cardio dataset.
-friedewald_cardio <- read.csv('data/JuistheidFRIE-Card(Remy)_251017_anonymised_wide_format.csv')
-friedewald_cardio_wo_missing <- friedewald_cardio %>%
+friedewald_cardio <- read.csv('data/JuistheidFRIE-Card(Remy)_251017_anonymised.csv')
+friedewald_cardio$X.3 <- as.numeric(friedewald_cardio$X.3)
+friedewald_cardio_wide <- pivot_wider(friedewald_cardio,
+                                  id_cols = c(ID,AfnD,AfnT,Gesl,Leeft ),
+                                  names_from = X,
+                                  values_from = X.3,
+                                  values_fn = list(X.3 = mean))
+friedewald_cardio_wide <- friedewald_cardio_wide %>%
+  dplyr::rename_with(~ gsub("-", ".", .x))
+friedewald_cardio_wo_missing <- friedewald_cardio_wide %>%
   filter(complete.cases(across(all_of(variables$selected_variables_friedewald)))) %>%
-  mutate(Gesl = replace_values(Gesl, "M" ~ 1, "V" ~ 0))
+  mutate(Gesl = recode(Gesl, "M" = 1, "V" = 0)) %>%
+  mutate(BezoekersID = row_number())
 
 write.csv(
   friedewald_cardio_wo_missing,
